6e743109f3f9a3ebc1138d66345484beedda46ea
Allan LE LAY <allan.le-lay@Digital-Grenoble.local>
2022-03-09 11:39:45 +0100

    adding footer + finalize product page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,4 +1,3 @@
-
 <!doctype html>
 <html lang="fr">
 <head>
@@ -7,8 +6,45 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Product</title>
     <link rel="stylesheet" href="/app.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans&display=swap" rel="stylesheet">
 </head>
 <body>
-    @yield('content')
+@yield('content')
 </body>
+<footer>
+    <div class="firstPartFoot">
+        <div class="listFooterGraduate">
+            <h3>Graduate</h3>
+
+            <ul>
+                <li>A propos</li>
+                <li>Ce que nous proposons</li>
+                <li>Direction</li>
+                <li>Carrières</li>
+                <li>Catalogue</li>
+                <li>Graduate Plus</li>
+                <li>Certificats professionnels</li>
+
+            </ul>
+        </div>
+        <div class="listFooterCommunaute">
+            <h3>Communauté</h3>
+
+            <ul>
+                <li>Étudiants</li>
+                <li>Partenaires</li>
+                <li>Développeurs</li>
+                <li>Béta testeur</li>
+                <li>Traducteurs</li>
+                <li>Blog Tech</li>
+                <li>Centre d'enseignement</li>
+            </ul>
+        </div>
+    </div>
+    <div class="favicon">
+        <p>&copy; 2022 Graduate Inc. Tous droits réservés.</p>
+    </div>
+</footer>
 </html>
