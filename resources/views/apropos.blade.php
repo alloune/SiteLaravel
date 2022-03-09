886488c8619fd56a1dc83acd3a6cf78e74830ee3
Mohamed LOTFI <mohamed.lotfi@Digital-Grenoble.local>
2022-03-09 16:20:40 +0100

    création de la page apropos

--- resources/views/apropos.blade.php
+++ resources/views/apropos.blade.php
@@ -5,15 +5,61 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Product</title>
-    <link rel="stylesheet" href="/app.css">
-    <link rel="stylesheet" href="/bahjat.css">
+    <link rel="stylesheet" href="{{ asset('/app.css') }}">
+    <link rel="stylesheet" href="{{ asset('/apropos.css') }}">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Open+Sans&display=swap" rel="stylesheet">
 </head>
 <body>
-<h1>test</h1>
-</body>
+<h1>Notre équipe</h1>
+<div class ='nous'>
+    <img src="{{ asset('/photo/allan.png') }}"
+          alt="Allan"/>
+
+    <img src="{{ asset('/photo/david.png') }}"
+         alt="Allan"/>
+    <img src="{{ asset('/photo/jp.png') }}"
+         alt="Allan"/>
+</div>
+<div class='nom'>
+    <p>Allan</p>
+    <p>David</p>
+    <p>J.P</p>
+</div>
+<div class='description'>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab aliquam amet animi at deleniti eius et explicabo
+        facere fugit in ipsam laudantium magni mollitia nam natus numquam omnis perspiciatis placeat porro provident
+        quaerat, quam similique tempore totam ut veniam voluptate. Accusantium doloribus iste nam natus officia quam
+        repudiandae ut. Quo?</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab aliquam amet animi at deleniti eius et explicabo
+    facere fugit in ipsam laudantium magni mollitia nam natus numquam omnis perspiciatis placeat porro provident
+    quaerat, quam similique tempore totam ut veniam voluptate. Accusantium doloribus iste nam natus officia quam
+    repudiandae ut. Quo?</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab aliquam amet animi at deleniti eius et explicabo
+    facere fugit in ipsam laudantium magni mollitia nam natus numquam omnis perspiciatis placeat porro provident
+    quaerat, quam similique tempore totam ut veniam voluptate. Accusantium doloribus iste nam natus officia quam
+    repudiandae ut. Quo?</p>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 <footer>
     <div class="firstPartFoot">
         <div class="listFooterGraduate">
