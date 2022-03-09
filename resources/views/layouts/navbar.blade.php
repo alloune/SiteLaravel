8ff411baaa72516cd82fe5354add6f8708845dee
Allan LE LAY <allan.le-lay@Digital-Grenoble.local>
2022-03-09 15:41:16 +0100

    named route and call it. If logo is clicked, go to homepage

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -2,7 +2,7 @@
 
 
 <div class="topnav">
-    <img src="{{ asset('Logo.svg') }}" alt="Logo">
+    <a class="logo" href={{route('home')}}><img src="{{ asset('Logo.svg') }}" alt="Logo"></a>
     <a style="background-color: #1A96D9; color: #ffffff"  href="#">Connexion</a>
     <a href="#">Mon panier</a>
     <a href="#">A propos</a>
