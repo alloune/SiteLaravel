ec2958d52566f787fe5270a5ad8cc2c2beddb7cd
bahjat tounsi <bahjattounsi1@hotmail.com>
2022-03-09 11:39:19 +0100

    Added top navbar bis

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -1,17 +1,10 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- Fonts -->
-    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="{{ asset('bahjat.css') }}">
 
-    <div class="topnav">
-        <img src="{{ asset('Logo.svg') }}">
-        <a href="#">Connexion</a>
-        <a href="#">Mon panier</a>
-        <a href="#">A propos</a>
-        <a href="#">Diplômes</a>
-    </div>
-</head>
+
+
+<div class="topnav">
+    <img src="{{ asset('Logo.svg') }}" alt="Logo">
+    <a href="#">Connexion</a>
+    <a href="#">Mon panier</a>
+    <a href="#">A propos</a>
+    <a href="#">Diplômes</a>
+</div>
