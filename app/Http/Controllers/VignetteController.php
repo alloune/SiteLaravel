7046c71de131ddaa5ac63aff74052fa481725d0e
Allan LE LAY <allan.le-lay@Digital-Grenoble.local>
2022-03-10 10:45:43 +0100

    update indent for array

--- app/Http/Controllers/VignetteController.php
+++ app/Http/Controllers/VignetteController.php
@@ -8,18 +8,18 @@ class VignetteController extends Controller
 {
     public $listCertif = array(
     "sorbonne" => array(
-    "name" => "logoSorbonne.png",
-    "description" => "Résolution de problèmes par méthodes computationnelles",
-    "backgroundImg" => "sorbBGImg"),
-        "milano" => array(
+            "name" => "logoSorbonne.png",
+            "description" => "Résolution de problèmes par méthodes computationnelles",
+            "backgroundImg" => "sorbBGImg"),
+    "milano" => array(
             "name" => "logoMilan.png",
             "description" => "DNA Decodificato",
             "backgroundImg" => "milanoBGImg"),
-        "bordeaux" => array(
+    "bordeaux" => array(
             "name" => "logoBordeaux.png",
             "description" => "MLOps,(Machine Learning Operations) Fundamentals",
             "backgroundImg" => "bordeauxBGImg"),
-        "stanford" => array(
+    "stanford" => array(
             "name" => "logoStan.png",
             "description" => "Applied data science with Python",
             "backgroundImg" => "stanfordBGImg"),
