da2f4c76d23db4f2ebadbd76f67e1a296d0542c8
Mohamed LOTFI <mohamed.lotfi@Digital-Grenoble.local>
2022-03-11 09:47:11 +0100

    enlever la redondance de la page about us

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,12 +6,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="{{ asset('app.css') }}"/>
     <link rel="stylesheet" href="{{ asset('bahjat.css') }}">
+    <link rel="stylesheet" href="{{ asset('apropos.css') }}">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Open+Sans&display=swap" rel="stylesheet">
     <title>@yield('title')</title>
 </head>
 <body>
+
 @include('layouts.navbar')
 @yield('content')
 
