69204c81884a36b7b52b368e37e8538669d3df76
bahjat tounsi <bahjattounsi1@hotmail.com>
2022-03-11 16:30:16 +0100

    refactoring

--- app/Http/Controllers/ProductDescriptionController.php
+++ app/Http/Controllers/ProductDescriptionController.php
@@ -5,8 +5,8 @@ namespace App\Http\Controllers;
 use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Contracts\View\Factory;
 use Illuminate\Contracts\View\View;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
+//use Illuminate\Http\Request;
+//use Illuminate\Support\Facades\DB;
 use App\Models\Products;
 
 class ProductDescriptionController extends Controller
@@ -14,7 +14,7 @@ class ProductDescriptionController extends Controller
     public function displayProduct(int $id): Factory|View|Application
     {
         $product = Products::where('id', $id)->get();
-        return view('product_description_db', ['product' => $product]) ;
+        return view('product_description_db', ['product' => $product]);
     }
 
 }
