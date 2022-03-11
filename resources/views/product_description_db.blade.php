a266228e7d9b53594eac7e2c2d6f70405c76894b
bahjat tounsi <bahjattounsi1@hotmail.com>
2022-03-11 15:19:30 +0100

    refactoring

--- resources/views/product_description_db.blade.php
+++ resources/views/product_description_db.blade.php
@@ -2,20 +2,19 @@
 @section('title', 'Product')
 @section('content')
 
-    <div class="teacherBlock">
-        <h2>Hello</h2>
-    </div>
 
-    <div class="globalDesc">
+
+    <div class="one_product">
 {{--        @dd($product[0])--}}
-
         @foreach($product as $key => $list)
-            <li>
-        <p>Nom du produit: {{ $list->name }}</p>
-            <img src="{{ $list->image }}">
-                <p>Prix: {{ $list->price }} €</p>
-            </li>
+            <div class="product_description">
+                <img src="{{ $list->image }}" alt="{{ $list->name }}">
+            </div>
+                <div class="product_description">
+                    <h2>Le produit {{ $list->name }}</h2>
+                    <h3>Prix: {{ $list->price }} €</h3>
+                    <a href="{{ route('products') }}" class="img_button">Ajouter au panier</a>
+                </div>
         @endforeach
-
     </div>
 @endsection
