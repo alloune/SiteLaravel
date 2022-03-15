b8bc90976754d56c4db488796a6c6d3771af5708
bahjat tounsi <bahjattounsi1@hotmail.com>
2022-03-15 16:17:02 +0100

    N/A

--- resources/views/Backoffice/backoffice.blade.php
+++ resources/views/Backoffice/backoffice.blade.php
@@ -2,19 +2,22 @@
 @section('title', "Backoffice")
 @section('content')
 
-    <table class="table" style="margin-top: 100px">
+    <table class="table table-hover" style="margin-top: 100px">
         <thead>
+        <tr><th scope="col"> <a href="{{ route('backofficeproductadd') }}"><button type="button" class="btn btn-success">Add product</button></a></th></tr>
         <tr>
             <th scope="col">id</th>
             <th scope="col">Name</th>
             <th scope="col">Description</th>
             <th scope="col">Price</th>
             <th scope="col">Quantity</th>
+            <th scope="col">Action</th>
+            <th scope="col">No going back</th>
         </tr>
         </thead>
+
         @foreach($products as $key => $list)
         <tbody>
-
         <tr class="{{ $list->quantity === 0 ? 'bg-warning': ''}}"> {{-- if condition (ternary condition --}}
 
             <th scope="row">{{ $list->id }}</th>
@@ -25,6 +28,8 @@
             <td>
                 <a href="{{ route('backofficeproduct', $list->id) }}"> <button type="button" class="btn btn-primary">View</button></a>
                 <a href="{{ route('backofficeproductedit', $list->id) }}"><button type="button" class="btn btn-primary">Update</button></a>
+                </td>
+            <td>
                 <form method="post" action="{{ route('backofficeproductsdelete', $list) }}">
                     @csrf
                     @method('delete')
@@ -35,11 +40,6 @@
 
         </tbody>
         @endforeach
-
     </table>
-
-    <a href="{{ route('backofficeproductadd') }}"><button type="button" class="btn btn-success">Add product</button></a>
-
-
 
 @endsection
