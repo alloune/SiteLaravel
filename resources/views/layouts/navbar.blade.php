2eff2953bc623952c8b138ebff2b468d157ef1e1
bahjat tounsi <bahjattounsi1@hotmail.com>
2022-03-21 13:16:15 +0100

    connexion working

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -3,9 +3,9 @@
 
 <div class="topnav">
     <a class="logo" href={{route('home')}}><img src="{{ asset('Logo.svg') }}" alt="Logo"></a>
-    <a style="background-color: #1A96D9; color: #ffffff"  href="{{ route('dashboard') }}">Connexion</a>
+    <a style="background-color: #1A96D9; color: #ffffff"  href="{{ route('backofficeproducts') }}">Connexion</a>
     <a href="#">Mon panier</a>
     <a href="{{route('aboutus')}}">A propos</a>
-    <a href="{{route('backofficeproducts')}}">BackOffice</a>
+{{--    <a href="{{route('backofficeproducts')}}">BackOffice</a>--}}
     <a href="{{route('products')}}">Diplômes</a>
 </div>
