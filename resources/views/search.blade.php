fa3391171d82252a3b7f2dc65e2e6b5e96aa0d25
bahjat tounsi <bahjattounsi1@hotmail.com>
2022-03-21 16:23:27 +0100

    Search function (not finished) bis

--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -3,17 +3,20 @@
 @section('content')
 
 
-
+<div style="margin-top: 100px">
     @if($products->isNotEmpty())
         @foreach ($products as $product)
-            <div class="post-list">
-                <p>{{ $product->name }}</p>
+            <div>
+                <h2>{{ $product->name }}</h2>
                 <img src="{{ $product->image }}">
             </div>
         @endforeach
     @else
         <div>
-            <h2>No posts found</h2>
+            <h2>No result found</h2>
         </div>
     @endif
+</div>
+
+
 @endsection
