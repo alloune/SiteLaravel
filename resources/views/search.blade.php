1a83ff890273c46b7be17a3ab862b5d9f2348e61
bahjat tounsi <bahjattounsi1@hotmail.com>
2022-03-21 16:17:33 +0100

    Search function (not finished)

---
+++ resources/views/search.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.app')
+@section('title', "Home")
+@section('content')
+
+
+
+    @if($products->isNotEmpty())
+        @foreach ($products as $product)
+            <div class="post-list">
+                <p>{{ $product->name }}</p>
+                <img src="{{ $product->image }}">
+            </div>
+        @endforeach
+    @else
+        <div>
+            <h2>No posts found</h2>
+        </div>
+    @endif
+@endsection
