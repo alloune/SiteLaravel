321d425c7f1719797c7edb0d625b386acaa281c9
bahjat tounsi <bahjattounsi1@hotmail.com>
2022-03-21 11:48:41 +0100

    Added reset password link the better way

--- routes/web.php
+++ routes/web.php
@@ -44,5 +44,3 @@ Route::put('/backoffice/product/edit/{product}',[BackofficeProductController::cl
 Route::prefix('backoffice')->group(function () {
     Route::resource('/order', BackofficeOrderController::class)->middleware(['auth']);
 });
-Route::get("message", "MessageController@formMessageGoogle");
-Route::post("message", "MessageController@sendMessageGoogle")->name('send.message.google');
